70452ae01392fef8fcd9cf129ea4982249768574
David Mueller <david.mueller1013@gmail.com>
2020-02-05 15:45:24 +0100

    add keymaster to passtrough
    Change-Id: I79d4e360dabc1c002bd3a0313d51821f135debeb

--- msm8974.mk
+++ msm8974.mk
@@ -136,9 +136,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
+    android.hardware.keymaster@3.0-impl
+    
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
