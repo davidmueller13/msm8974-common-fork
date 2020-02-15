a5937e0c37c8a8aeecf83f1ad7e9fb102b866d9b
David Mueller <david.mueller1013@gmail.com>
2020-02-15 12:40:26 +0100

    add charger
    Change-Id: I80d967a896e595a5ea41996dba72166deb491697

--- config.fs
+++ config.fs
@@ -7,6 +7,12 @@ value:2951
 [AID_VENDOR_RFS_SHARED]
 value:2952
 
+[/system/bin/charger]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [efs/]
 mode: 0771
 user: AID_SYSTEM
