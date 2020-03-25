b11840fe58c81c6e1827ca12b7eab5555df057f4
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2020-03-25 19:04:49 +0300

    msm8974-common: drm: Enable clearkey 1.2 drm plugin
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -115,7 +115,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # GPS
 PRODUCT_PACKAGES += \
